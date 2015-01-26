6902f19d705ed06d204decbd9e2162ac879d6a9b
Ganapati V S <vsg.inbox@gmail.com>
2015-01-26 20:42:33 +0530

    Now playing image + Text

--- js/script.js
+++ js/script.js
@@ -2,6 +2,17 @@
  * Created by GuNs on 18-01-2015.
  */
 
+if (window.ShadowDOMPolyfill) {
+    // blunt hack for ShadowDOM Polyfill inability to wrap `document` and `document.body`
+    jQuery.fn._init = jQuery.fn.init;
+    jQuery.fn.init = function(selector, context) {
+        if (selector && selector.nodeType) {
+            selector = ShadowDOMPolyfill.wrapIfNeeded(selector);
+        }
+        return jQuery.fn._init(selector, context);
+    }
+}
+
 //Preloading js
 $(function($) {
     var cache = [];
